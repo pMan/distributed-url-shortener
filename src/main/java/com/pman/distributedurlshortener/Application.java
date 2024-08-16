806970046c408d329cb8259cae2d9d50f1215217
Prasad Cholakkottil <prasadcnair@gmail.com>
2024-08-15 23:32:38 -0500

    add pproperties file, web server logging

--- src/main/java/com/pman/distributedurlshortener/Application.java
+++ src/main/java/com/pman/distributedurlshortener/Application.java
@@ -12,27 +12,38 @@ import com.pman.distributedurlshortener.zk.ZooKeeperClient;
 
 public class Application {
 
+    private static Properties properties = new Properties();
+
+    static {
+        try {
+            properties.load(Application.class.getClassLoader().getResourceAsStream("application.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
     public static void main(String[] args) throws IOException, KeeperException, InterruptedException {
 
-        System.out.println("\nStarting Distributed URL shortener\n");
-        Properties props = Defaults.loadDefault();
         if (args.length == 1) {
             try {
                 System.out.println("Loading properties file: " + args[0]);
-                props.load(new FileInputStream(args[0]));
+                properties.load(new FileInputStream(args[0]));
             } catch (IOException e) {
                 e.printStackTrace();
                 System.exit(1);
             }
         }
 
-        for (Entry<Object, Object> entry : props.entrySet()) {
-            System.out.println(String.format("%-30s", entry.getKey()) + ": " + entry.getValue());
+        System.out.println(
+                "Starting Distributed URL shortener!\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        for (Entry<Object, Object> entry : properties.entrySet()) {
+            System.out.println(String.format("%-36s", entry.getKey()) + ": " + entry.getValue());
         }
 
-        String hostport = props.getProperty("zk.hostport");
-        int timeout = (int) props.get("zk.session.timeout");
-        String port = (String) props.get("http.server.port");
+        String hostport = properties.getProperty("zookeeper.hostport");
+        int timeout = Integer.parseInt(properties.getProperty("zookeeper.sessiontimeout.ms"));
+        String port = properties.getProperty("http.server.port");
 
         ZooKeeperClient zooKeeperClient = new ZooKeeperClient(hostport, timeout, port);
         WebServer httpServer = new WebServer(port, zooKeeperClient);
@@ -42,4 +53,7 @@ public class Application {
         zooKeeperClient.waitTillDisconnected();
     }
 
+    public static Properties getProperties() {
+        return Application.properties;
+    }
 }
