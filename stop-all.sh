00d7fc86b8c1e62caec529f13a79457b30f1181c
Prasad Cholakkottil <prasadcnair@gmail.com>
2024-09-19 22:42:28 -0500

    script comments

--- stop-all.sh
+++ stop-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # stop all app instances
 
-# INT is enough to trigger the registered shutdown hook, feel free to use KILL
+# SIG_INT is perfect to trigger the registered shutdown hook.
 ARG='INT'
 
 ps -ef | grep 'distributed-url-shortener.*\.jar' | grep -v grep | awk '{print $2}' | xargs kill -$ARG
